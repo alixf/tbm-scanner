751828bfe1fdf2212e3f1e306c042c91214c63e7
Alix <alix.fumoleau@gmail.com>
2018-03-01 23:27:00 +0100

    Add the first implementation

---
+++ index.js
@@ -0,0 +1,51 @@
+import * as React from "react";
+
+export class TBMScanner extends React.Component {
+	constructor(props) {
+		super(props);
+		this.fetchData();
+		setInterval(this.updateData, 1000);
+		this.trips = [];
+		this.stopPoints = [];
+		this.index = 0;
+	}
+
+	fetchData = async () => {
+		const response = await fetch(`https://ws.infotbm.com/ws/1.0/network/line-informations/line:TBC:${this.props.lineId}`);
+		const result = await response.json();
+		this.stopPoints = result.routes[0].stopPoints.map(stopPoint => {
+			return stopPoint.id.split(":")[3];
+		});
+	}
+	
+	updateData = async () => {
+		if(!this.stopPoints) {
+			console.log("no stop points");
+			return;
+		}
+		this.index = (this.index + 1) % this.stopPoints.length;
+		const stopId = this.stopPoints[this.index];
+		const response = await fetch(`https://ws.infotbm.com/ws/1.0/get-realtime-pass/${stopId}/${this.props.lineId}`);
+		const result = await response.json();
+		for(const destinationId in result.destinations) {
+			for(const trip of result.destinations[destinationId]) {
+				const tripIndex = this.trips.findIndex(it => it.trip_id === trip.trip_id);
+				if (tripIndex === -1) {
+					this.trips.push(trip);
+					if(this.props.onNewBus) {
+						this.props.onNewBus(trip);
+					}
+				} else {
+					this.trips[tripIndex] = trip;
+					if(this.props.onBusUpdate) {
+						this.props.onBusUpdate(trip);
+					}
+				}
+			}
+		}
+	}
+
+	render() {
+		return null;
+	}
+}
